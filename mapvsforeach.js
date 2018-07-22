172e09d1e7a8b354c540e0643864f3204cd49d58
Aadhithyan Karthick <aadhithyankarthick@gmail.com>
2018-07-22 23:11:18 +0530

    mapvsforeach.js

--- mapvsforeach.js
+++ mapvsforeach.js
@@ -2,10 +2,10 @@
 var a=[2,4,7,8];
 const new_a=a.map(function (num) {
 	if (num%2==0) {
-        num=num*2;
+        num=num*3;
 	}
 	else{
-        num=num*3;
+        num=num*2;
 	}
  return num;   
 });
@@ -13,9 +13,10 @@ console.log(new_a);
 
 
 var b=[-1,-2,4,5];
-const new_b=b.map(function (no) {
-    if(no<0 & typeof no !=='undefined'){
-        return no;
+var new_b=[];
+b.forEach(function (no) {
+    if(no<0){
+     !!no && new_b.push(no);
     }
 });
 console.log(new_b);
