a3bd7d12b7fa808e2bf81ffee0d2173d23075b27
Aadhithyan Karthick <aadhithyankarthick@gmail.com>
2018-07-26 00:56:35 +0530

    reactJS tutorial

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from  'react-dom';
+const Header=((props) => {
+	return(
+		<div>
+		<h2 className="style">{props.headerbrand}</h2>
+		</div>
+		)
+	
+})
+const Content=(()=>{
+	const name=["karthick","aadhithyan","john","cooper"]
+return(
+		<div>
+			<ul>
+				{name.map( (n,i) => {
+					return(
+						<li key={i}>{n}</li>
+						)
+				} 	
+				)}
+			</ul>
+		</div>
+	)
+	})
+const Welcome=(() => {
+		return(
+		<div>
+		<Header headerbrand="HomeAutomation" />
+		<Content />
+		</div>
+		)
+	
+});
+ReactDom.render(
+	<Welcome />,
+	document.getElementById('root')
+	);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
