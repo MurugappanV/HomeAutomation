4ac04f0faba9853b9c4e356339e41a03dbb0f3c1
Aadhithyan Karthick <aadhithyankarthick@gmail.com>
2018-08-05 13:34:46 +0530

    not working

--- myapp/src/Light.js
+++ myapp/src/Light.js
@@ -1,4 +1,10 @@
 import React,{Component} from 'react';
+import Bulboff from './img/bulboff.jpg';
+import Bulbon from './img/bulbon.jpg';
+let b_on=document.getElementById('bulboff');
+const changebulb=()=>{
+	b_on.src={Bulbon};
+}
 
 class Light extends Component{
 	render(){
@@ -6,9 +12,10 @@ class Light extends Component{
 			 <div>
         <h2>Automate your house lights</h2>
         <p>We provide home automation for lights like increasing the brightness for your mood and automatically change the color of lights</p>
- 
+ 		
         <p>Home automation at low price</p>
-      </div>
+        <img src={Bulboff} id="bulboff" alt="bulboff" onClick="changebulb()" />
+         </div>
 			);
 	}
 }
