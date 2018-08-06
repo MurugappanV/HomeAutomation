c9c2b299ef2d1d9e4e133fa13e3f0d80cd8314db
Aadhithyan Karthick <aadhithyankarthick@gmail.com>
2018-08-07 00:21:00 +0530

    multiple bulb partial completed

--- myapp/src/Light.js
+++ myapp/src/Light.js
@@ -2,11 +2,6 @@ import React,{Component} from 'react';
 import Bulboff from './img/bulboff.jpg';
 import Bulbon from './img/bulbon.jpg';
 
-// let changebulb=()=>{
-// 	let b_on=document.getElementById('bulboff');
-// 	b_on.src={Bulbon};
-// }
-
 class Light extends Component{
     constructor(props) {
         super(props)
@@ -19,19 +14,28 @@ class Light extends Component{
         } else {
             return <img src={Bulboff} id="bulboff" alt="bulboff" onClick={() => this.setState({lightOn : true})} />
         }
-    } 
-
-	render(){
-		return(
-			 <div>
+    }
+  
+   
+render(){
+	let row=[];
+    for(var i=0;i<4;i++){
+    	row.push(this.renderBulb());
+    }
+    return(
+       <div>
         <h2>Automate your house lights</h2>
         <p>We provide home automation for lights like increasing the brightness for your mood and automatically change the color of lights</p>
- 		
+     
         <p>Home automation at low price</p>
-        {this.renderBulb()}
+
+        
+        	{row}
+        	
+       
          </div>
-			);
-	}
+      );
+  }
 }
 
 export default Light;
