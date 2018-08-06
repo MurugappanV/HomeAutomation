63996743a1b07c0be45315dfa60266373c5e3d2e
Murugappan <murugappanviswanathan@gmail.com>
2018-08-06 23:11:43 +0530

    Light changes

--- myapp/src/Light.js
+++ myapp/src/Light.js
@@ -2,12 +2,25 @@ import React,{Component} from 'react';
 import Bulboff from './img/bulboff.jpg';
 import Bulbon from './img/bulbon.jpg';
 
-let changebulb=()=>{
-	let b_on=document.getElementById('bulboff');
-	b_on.src={Bulbon};
-}
+// let changebulb=()=>{
+// 	let b_on=document.getElementById('bulboff');
+// 	b_on.src={Bulbon};
+// }
 
 class Light extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {lightOn : false}
+    }
+
+    renderBulb = () => {
+        if(this.state.lightOn) {
+            return <img src={Bulbon} id="bulbon" alt="bulbon" onClick={() => this.setState({lightOn : false})} />
+        } else {
+            return <img src={Bulboff} id="bulboff" alt="bulboff" onClick={() => this.setState({lightOn : true})} />
+        }
+    } 
+
 	render(){
 		return(
 			 <div>
@@ -15,7 +28,7 @@ class Light extends Component{
         <p>We provide home automation for lights like increasing the brightness for your mood and automatically change the color of lights</p>
  		
         <p>Home automation at low price</p>
-        <img src={Bulboff} id="bulboff" alt="bulboff" onClick={this.changebulb} />
+        {this.renderBulb()}
          </div>
 			);
 	}
