cf77c3c5ea362eb28f46791b8f92b722eeea185e
Aadhithyan Karthick <aadhithyankarthick@gmail.com>
2018-08-12 16:31:52 +0530

    fan js

--- myapp/src/Fan.js
+++ myapp/src/Fan.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-
+import wing from './img/fan.png';
 class Fan extends Component{
 	render(){
 		return(
@@ -7,6 +7,7 @@ class Fan extends Component{
         <h2>Automate your Fan for wind</h2>
         <p>We provide home automation for fans</p> 
         <p>Home automation at low price</p>
+        <img src={wing} width='300px' height='300px' alt='fan' className='rotate' />
       </div>
 			);
 	}
